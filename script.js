8149c921d0b995184463d35fa10589ab58d3ddc6
Umme Suleyath <ummesul76@gmail.com>
2021-12-27 16:18:45 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('This is a testing connectivity')
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
+let apiQuotes = []
+
+// show new quote
+function newQuote() {
+    // Pick random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    console.log(authorText.textContent)
+    //checking if the author is null
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+    //changing the quote style based onthe quote length
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
+
+}
+
+// get quotes from api
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes'
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json();
+        // console.log(apiQuotes)
+        newQuote();
+    } catch (error) {
+
+        console.log(error)
+    }
+
+}
+//OnLoad
+getQuotes();
+
